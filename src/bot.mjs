9e521d5fb3fb9c84047154e38c08dd678cd15892
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-05-25 18:42:14 +0500

    Test

--- src/bot.mjs
+++ src/bot.mjs
@@ -1,7 +1,39 @@
-import {Bot} from "grammy";
+import {Bot, session} from "grammy";
+import {freeStorage} from "@grammyjs/storage-free";
+import {conversations, createConversation,} from "@grammyjs/conversations";
 
 export const bot = new Bot(process.env.TELEGRAM_BOT_TOKEN);
 
+bot.use(session({
+    initial: () => ({count: 0}),
+    storage: freeStorage(bot.token),
+}));
+
+bot.use(conversations());
+
+bot.use(createConversation(test));
+
+bot.command("cancel", async ctx => {
+    await ctx.conversation.exit();
+    await ctx.reply("Leaving.");
+});
+
+bot.command("test", async ctx => ctx.conversation.enter("test"));
+
 bot.on("message:text", async ctx => ctx.reply(ctx.msg.text));
+
+async function test(conversation, ctx) {
+    await ctx.reply("How many favorite movies do you have?");
+    const count = await conversation.form.number();
+    const movies = [];
+    for (let i = 0; i < count; i++) {
+        await ctx.reply(`Tell me number ${i + 1}!`);
+        const titleCtx = await conversation.waitFor(":text");
+        movies.push(titleCtx.msg.text);
+    }
+    await ctx.reply("Here is a better ranking!");
+    movies.sort();
+    await ctx.reply(movies.map((m, i) => `${i + 1}. ${m}`).join("\n"));
+}
 
 export default bot;
